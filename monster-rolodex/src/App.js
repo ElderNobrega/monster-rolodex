30ff84404e74745d8b6981011065dc9c73115563
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-08-11 22:40:27 -0400

    card list component and css stylesheet

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,34 +1,30 @@
 import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      monster: [
-        {
-          name: "Frankenstein",
-          id: "01"
-        },
-        {
-          name: "Dracula",
-          id: "02"
-        },
-        {
-          name: "Zombie",
-          id: "03"
-        },
-      ]
+      monster: []
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => this.setState({monster: users}))
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monster.map(monster => (
+        <CardList name="Elder">
+          {this.state.monster.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
+        </CardList>
       </div>
     );
   }
