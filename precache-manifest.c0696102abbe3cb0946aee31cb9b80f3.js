ae34a8d9a4dcc5e7c156e68e0e5b1b8ea3a76874
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-08-13 00:51:45 -0400

    Updates

---
+++ precache-manifest.c0696102abbe3cb0946aee31cb9b80f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9931c5836c91f086e9b14041c8941bb9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "375bf6682e9cf714637c",
+    "url": "/monster-rolodex/static/css/main.51b10a04.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375bf6682e9cf714637c",
+    "url": "/monster-rolodex/static/js/main.1fa0ee70.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
